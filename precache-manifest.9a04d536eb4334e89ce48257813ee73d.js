bedcf7efecb61eae020bba1210dbe5b10b4edf9d
alinamoto <95858189+alinamoto@users.noreply.github.com>
2022-03-25 15:44:13 +0200

    Updates

---
+++ precache-manifest.9a04d536eb4334e89ce48257813ee73d.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "69302b1c5b9c9c0835cb",
+    "url": "/kittyland/static/css/main.7fd5f991.chunk.css"
+  },
+  {
+    "revision": "69302b1c5b9c9c0835cb",
+    "url": "/kittyland/static/js/main.69302b1c.chunk.js"
+  },
+  {
+    "revision": "7d8a5b69bced8be18895",
+    "url": "/kittyland/static/js/1.7d8a5b69.chunk.js"
+  },
+  {
+    "revision": "20c9cb8af352aad016de",
+    "url": "/kittyland/static/css/2.871315ca.chunk.css"
+  },
+  {
+    "revision": "20c9cb8af352aad016de",
+    "url": "/kittyland/static/js/2.20c9cb8a.chunk.js"
+  },
+  {
+    "revision": "37308ed035f7d2b81bbc",
+    "url": "/kittyland/static/js/runtime~main.37308ed0.js"
+  },
+  {
+    "revision": "8f57dbcef9e492fec48d2101a8e40a08",
+    "url": "/kittyland/static/media/Art-Brewery.8f57dbce.ttf"
+  },
+  {
+    "revision": "9237412db43660787df482fa884ebcfc",
+    "url": "/kittyland/static/media/Candy-Shop.9237412d.ttf"
+  },
+  {
+    "revision": "633265b0022981d41ba1d37025a8d159",
+    "url": "/kittyland/index.html"
+  }
+];
